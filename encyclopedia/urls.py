f244b18ccbe2c7fd31a674705411c91c19959bbb
TheKingbyKey <42425810+TheKingbyKey@users.noreply.github.com>
2020-07-12 15:23:47 +0530

    added wiki page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<string:title>",views.page,name="page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
@@ -8,3 +8,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request,title):
+    md = markdown2.Markdown()
+    html = md.convert(util.get_entry(title))
+    return render(request,"encyclopedia/page.html",{"html":html})
